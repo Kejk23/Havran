1bc3756bf78854d5be811b2024b7751c1e9fd9f5
Kejk23 <mikikuba6@gmail.com>
2022-09-02 12:44:46 +0200

    $@

---
+++ airsim_functions/captureImages.py
@@ -0,0 +1,120 @@
+import airsim
+import time
+import os
+import numpy as np
+import cv2
+import redis
+import argparse
+
+conn = redis.Redis(host='localhost', port=6379)
+MAX_IMAGES = 50
+
+# set the camera pose of the drone while flying
+def setCameraPose(client):
+    camera_pose = airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(-1.5708, 0, 0))
+    client.simSetCameraPose(1, camera_pose)
+
+# add real time images captured by drone to the redis stream
+def addToStream(conn,img_rgb,iteration,imagename,maxImages):
+    _, data = cv2.imencode('.jpg', img_rgb)
+    storedimg = data.tobytes()
+    iteration.append(['imagename',imagename])
+    iteration.append(['image',storedimg])
+    iteration.append(['isDone','0'])
+    conn.execute_command('xadd', 'inspectiondata',  'MAXLEN', '~', str(maxImages), '*', *sum(iteration, []))
+
+# get the images of the land taken by drone
+def getRealTimeImage(client,count):
+    temp_dir = os.path.join(os.getcwd(), "airsim_drone")
+    filepath = os.path.join(temp_dir, str(count))
+    simImages = client.simGetImages([airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])
+    simImage = simImages[0]
+    img1d = np.fromstring(simImage.image_data_uint8, dtype=np.uint8)
+    img_rgb = img1d.reshape(simImage.height, simImage.width, 3)
+    return img_rgb
+        
+def convertToMap(data):
+    if isinstance(data, bytes):  return data.decode('ascii')
+    if isinstance(data, dict):   return dict(map(convertToMap, data.items()))
+    if isinstance(data, tuple):  return map(convertToMap, data)
+    return data
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--level', help='Game Level', type=int, default=1)
+    args = parser.parse_args()
+
+    # set the weather conditions for different game levels
+    gameLevel = args.level
+    print(gameLevel)
+    input = []
+    if gameLevel == 3:    
+        print('Level 3')
+        input.append(['weather','Sunny'])
+        input.append(['windSpeed',25])
+    elif gameLevel == 2:
+        print('Level 2')
+        input.append(['weather','Sunny'])
+        input.append(['windSpeed',5])
+    else:
+       print('Level 1')
+       input.append(['weather','Rain'])
+       input.append(['windSpeed',5])
+
+    # creating the consumer group if it does not exist to read the data from the stream
+    try:
+        conn.execute_command('xgroup','CREATE','inspection','InspectionGroup','$','MKSTREAM')
+    except:
+        print("Consumer Group already  exist")
+   
+    # waiting to receive input the redis stream and once the signal is received drone starts flying and stores real time images to Redis Stream in form of bytes.  
+    try:
+        client = airsim.VehicleClient()
+        client.confirmConnection()
+        while True:
+            res = conn.execute_command('xreadgroup','GROUP', 'InspectionGroup','InspectionConsumer','Block', 10000,'STREAMS', 'inspection','>')
+            if res:
+                print("Signal received from stream")
+                    
+                count=1
+                currentStream = res[0][1][0]
+                currentStreamMap = convertToMap(currentStream)
+                currentStreamMapList = list(currentStreamMap)
+                streamID = currentStreamMapList[0]
+                inspectionId = currentStreamMapList[1]['inspectionId']
+                print("Inspection ID is " + inspectionId )
+                print("Stream ID is " + streamID )
+
+                setCameraPose(client)
+
+                client.confirmConnection()
+                client.enableApiControl(True)
+                client.armDisarm(True)
+
+                print("Drone is ready to fly now")
+
+                if client.isApiControlEnabled():
+                    res = conn.execute_command('xack','inspection','InspectionGroup',streamID)
+                    print("Stream Acknowledged " + str(res))
+
+                while(client.isApiControlEnabled()):
+                    iteration = input[:]
+                    imagename = inspectionId + "_" + str(count) + '.jpg'
+                    img_rgb = CaptureImageService.getRealTimeImage(client,count)
+                    addToStream(conn,img_rgb,iteration,imagename,MAX_IMAGES)
+                    time.sleep(2)
+                    print(count)
+                    count += 1
+
+                lastRow = input[:]
+                lastRow.append(['isDone','1'])
+                lastRow.append(['imagename',''])
+                lastRow.append(['image',''])
+                print("Saving Final Row")
+                conn.execute_command('xadd', 'inspectiondata',  'MAXLEN', '~', str(MAX_IMAGES), '*', *sum(lastRow,[]))
+            else:
+                print("Yet no input from stream")
+    except:
+        print("Airsim is not yet ready")
+    
--- airsim_functions/flyDrone.py
+++ airsim_functions/flyDrone.py
@@ -1,35 +1,133 @@
 import airsim
 import argparse
-from flyDroneService import FlyDroneService
+import pprint
 import time
+import numpy as np
+import math
 
+def flyOverRectangleArea(corners, spacing, height, velocity):
+    corners = corners[corners[:, 0].argsort()]
+    firstVector = [corners[0][0]-corners[1][0],
+    corners[0][1]-corners[1][1]]
+    secondVector = [corners[0][0]-corners[2][0],
+    corners[0][1]-corners[2][1]]
+    length = math.sqrt(firstVector[0]**2 + firstVector[1]**2)
+    unitVecotr = firstVector / length
+    partialVector = unitVecotr * spacing
+    steps = length / spacing
+    cords = [[corners[0][0], corners[0][1]], [corners[2][0], corners[2][1]]]
+    for x in range(steps):
+        cords[2 + x] = cords[1 + x] + firstVector
+        cords[3 + x] = cords[2 + x] + secondVector
+    for x in range(len(cords)):
+        client.moveToPositionAsync(*cords, height, velocity)
+    
+def resetAirSimClient(client):
+    client.hoverAsync().join()
+    client.armDisarm(False)
+    client.reset()
+    client.enableApiControl(False)
+    
+def initializeAirSimClient(client):
+
+    while not client.isApiControlEnabled():
+        print("Client is waiting to take off")
+        time.sleep(1)
+
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+
+    state = client.getMultirotorState()
+    s = pprint.pformat(state)
+    print("state: %s" % s)
+
+    imu_data = client.getImuData()
+    s = pprint.pformat(imu_data)
+    print("imu_data: %s" % s)
+
+    barometer_data = client.getBarometerData()
+    s = pprint.pformat(barometer_data)
+    print("barometer_data: %s" % s)
+
+    magnetometer_data = client.getMagnetometerData()
+    s = pprint.pformat(magnetometer_data)
+    print("magnetometer_data: %s" % s)
+
+    gps_data = client.getGpsData()
+    s = pprint.pformat(gps_data)
+    print("gps_data: %s" % s)
+
+    client.takeoffAsync().join()
+
+    state = client.getMultirotorState()
+    print("state: %s" % pprint.pformat(state))
+    
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--level', help='Game Level', type=int, default=1)
+    parser.add_argument('--level', help='Game Level', type=int, default=1)
     args = parser.parse_args()
     
     # connect to the AirSim simulator
     client = airsim.MultirotorClient()
-    FlyDroneService.initializeAirSimClient(client)
+    initializeAirSimClient(client)
 
     gameLevel = args.level
-    print(gameLevel)
+    print('Level' + str(gameLevel))
+    client.simEnableWeather(True)
+        
     # set the coordinates to set up the path for drone to travel
     if gameLevel == 3:
-        print('Level 3')
-        client.simEnableWeather(True)
-        FlyDroneService.setFlyingCoordsForDroneAtThirdLevel(client)
-    elif gameLevel == 2:
-        print('Level 2')
-        client.simEnableWeather(True)
-        FlyDroneService.setFlyingCoordsForDroneAtSecondLevel(client)
-    else:
-        print('Level 1')
-        client.simEnableWeather(True)
-        client.simSetWeatherParameter(airsim.WeatherParameter.Rain, 1)
-        FlyDroneService.setFlyingCoordsForDroneAtFirstLevel(client)
+        #flyOverRectangleArea(np.array([0, -80], [0, -9], [-98, -80], [-98, -9]), 17, -1, 10)
+            
+        client.moveToPositionAsync(0,-90, -1, 10).join()
+        client.moveToPositionAsync(-20,-90,-1, 10).join()
+        client.moveToPositionAsync(-20, -9, -1,10).join()
+        client.moveToPositionAsync(-40, -9, -1, 10).join()
+        client.moveToPositionAsync(-40, -80, -1,10).join()
+        client.moveToPositionAsync(-54, -80, -1, 10).join()
+        client.moveToPositionAsync(-54, -9, -1, 10).join()
+        client.moveToPositionAsync(-74, -9, -1, 10).join()
+        client.moveToPositionAsync(-74, -80, -1, 10).join()
+        client.moveToPositionAsync(-88, -80, -1, 10).join()
+        client.moveToPositionAsync(-88, -9, -1, 10).join()
+        client.moveToPositionAsync(-98, -9, -1, 10).join()
+        client.moveToPositionAsync(-98, -80, -1, 10).join()
+    
+    if gameLevel == 2:
+        #flyOverRectangleArea(np.array([0, -90], [0, -5], [-98, -90], [-98, -5]), 17, -2, 8)
+
+        client.moveToPositionAsync(0,-102, -2, 8).join()
+        client.moveToPositionAsync(-28,-92,-2, 8).join()
+        client.moveToPositionAsync(-28, -5, -2,8).join()
+        client.moveToPositionAsync(-39, -5, -2, 8).join()
+        client.moveToPositionAsync(-39, -90, -2,8).join()
+        client.moveToPositionAsync(-59, -90, -2, 8).join()
+        client.moveToPositionAsync(-59, -5, -2, 8).join()
+        client.moveToPositionAsync(-78, -5, -2, 8).join()
+        client.moveToPositionAsync(-78, -90, -2, 8).join()
+        client.moveToPositionAsync(-88, -90, -2, 8).join()
+        client.moveToPositionAsync(-88, -5, -2, 8).join()
+        client.moveToPositionAsync(-98, -5, -2, 8).join()
+        client.moveToPositionAsync(-98, -90, -2, 8).join()
+    
+    if gamelevel == 1:
+        #flyOverRectangleArea(np.array([0, -90], [0, -9], [-98, -78], [-98, -9]), 17, 0, 10)
+
+        client.moveToPositionAsync(0,-85, 0, 10).join()
+        client.moveToPositionAsync(-14,-85,0, 10).join()
+        client.moveToPositionAsync(-14, -9, 0, 10).join()
+        client.moveToPositionAsync(-30, -9, 0, 10).join()
+        client.moveToPositionAsync(-30, -78, 0, 10).join()
+        client.moveToPositionAsync(-48, -78, 0, 10).join()
+        client.moveToPositionAsync(-48, -9, 0, 10).join()
+        client.moveToPositionAsync(-65, -9, 0, 10).join()
+        client.moveToPositionAsync(-65, -78, 0, 10).join()
+        client.moveToPositionAsync(-80, -78, 0, 10).join()
+        client.moveToPositionAsync(-80, -9, 0, 10).join()
+        client.moveToPositionAsync(-98, -9, 0, 10).join()
+        client.moveToPositionAsync(-98, -78, 0, 10).join()
         
     #stop and return the simulator to initial state
-    FlyDroneService.resetAirSimClient(client)
-
+    resetAirSimClient(client)
